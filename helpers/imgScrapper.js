65c4457e6fb1eb530e789c7d3c4566cb98ee91bd
Albert <albpfau@gmail.com>
2020-08-27 18:30:27 +0300

    regex

--- helpers/imgScrapper.js
+++ helpers/imgScrapper.js
@@ -1,24 +1,24 @@
-let express = require('express');
-let app = express();
-let request = require('request');
-let cheerio = require('cheerio');
-let port = 8080;
+// let express = require('express');
+// let app = express();
+// let request = require('request');
+// let cheerio = require('cheerio');
+// let port = 8080;
 
-let url = "https://ru.inshaker.com/cocktails/90-aviatsiya";
+// let url = "https://ru.inshaker.com/cocktails/90-aviatsiya";
 
-let content = [];
+// let content = [];
 
-request(url, function(err, res, body) {
-  let $ = cheerio.load(body);
-    $('enclosure').each(function(i, elem) {
-    	content.push($(this).attr('url'));
-   });
-    for (let i = 0; i<content.length; i++) {     
-      console.log(content[i]);   
-    }
-});
+// request(url, function(err, res, body) {
+//   let $ = cheerio.load(body);
+//     $('enclosure').each(function(i, elem) {
+//     	content.push($(this).attr('url'));
+//    });
+//     for (let i = 0; i<content.length; i++) {     
+//       console.log(content[i]);   
+//     }
+// });
 
-app.listen(port);
+// app.listen(port);
 
 
-module.exports = { request() }
+// module.exports = { request() }
