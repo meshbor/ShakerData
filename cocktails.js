a2b41c99c4a6306876a468dc43da55264a964ca8
Albert <albpfau@gmail.com>
2020-08-27 11:36:43 +0300

    add coct

--- cocktails.js
+++ cocktails.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose')
-
+mongoose.connect('mongodb://localhost/cocktailBase', {useNewUrlParser: true, useUnifiedTopology:true});
 const Cocktail = require('./models/cocktail')
+
+
+async function changeCockt(){
+  const cockt  = await Cocktail.findOne({})
+  const newCockt = cockt.toString()
+  newCockt.split( )
+
+
+}
+changeCockt();
+
