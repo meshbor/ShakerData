f95adb626c28ffa3f11ef7e246d52d782a0a73ae
Eva <schennikova.ev@gmail.com>
2020-08-27 11:34:54 +0300

    new Router

---
+++ public/js/preview.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('#serchTitle')
+//console.log(serch)
+console.log(123);
+input.addEventListener('submit', (e) => {
+  //e.preventDefault()
+  console.log(e.target);
+
+  const v = e.target.name.value
+})
