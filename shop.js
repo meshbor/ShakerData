c229938ddf39a7074852d689aa1932d0b417bea1
Boris <mesh.boris@mail.ru>
2020-08-28 17:27:03 +0300

    Release with working shops

--- shop.js
+++ shop.js
@@ -37,12 +37,12 @@ let arrayofValue = [
   ['Белый русский',10],
   ['Дайкири',10],
   ['Кровавая Мэри',10],
-  ['Московский мул',1100],
+  ['Московский мул',12],
  
 ]
 
 async function fullOrder(arrayofValue) {
-
+  let volumeOrder = 0;
 
 for (let i = 0; i < arrayofValue.length; i++) {
   let yourOrder = await Cocktail.findOne({ title: `${arrayofValue[i][0]}`})
@@ -51,8 +51,9 @@ for (let i = 0; i < arrayofValue.length; i++) {
 
   for (let j = 0; j < ingredientsAsk.length; j++) {
     let BasaIngred =  await Order.findOne({ ingredients: `${ingredientsAsk[j][0]}`})
+    console.log(BasaIngred);
      if (BasaIngred === null) { //если такого ингредиента еще в базе нет, то создаем его
-        let volumeOrder = ingredientsAsk[j][1]*arrayofValue[i][1];
+        volumeOrder = ingredientsAsk[j][1]*arrayofValue[i][1];
         const order = await new Order({
         ingredients : ingredientsAsk[j][0],
         volume: volumeOrder,
@@ -61,8 +62,9 @@ for (let i = 0; i < arrayofValue.length; i++) {
       console.log(order);
       await order.save();
      } else {
-       const res = await Order.update( {ingredients : `Водка Finlandia` }, {$set: {volume:`${}`}} ) 
        //иначе если такой ингредиент есть 
+      let volumer = volumeOrder + (ingredientsAsk[j][1] * arrayofValue[i][1]);
+       const res = await Order.update( {ingredients : `${ingredientsAsk[j][0]}` }, {$set: {volume:`${volumer}`}} ) 
 
       console.log('tuta');
      }
