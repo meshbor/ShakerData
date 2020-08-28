413e9f73d2cb5694a98149ecf47b0f0ef485e632
Eva <schennikova.ev@gmail.com>
2020-08-28 12:48:15 +0300

    addchoosen

--- routes/preview.js
+++ routes/preview.js
@@ -6,7 +6,7 @@ const Cocktail = require('../models/cocktail.js')
 // mongoose.connect('mongodb://localhost/cocktailBase', { useNewUrlParser: true, useUnifiedTopology: true });
 router.route('/')
   .get(async (req, res) => {
-    const allCocktall = await Cocktail.find({},{_id: 0})
+    const allCocktall = await Cocktail.find({})
     res.render('coctail/preview', { allCocktall })
   })
   //.limit(10){title:`${ qweryInSerch }`}
@@ -22,9 +22,31 @@ router.route('/')
     console.log(selectCocktail);
     // console.log(selectCocktailIngrdients);
     // console.log(selectCocktailRecipe);
+    res.status('200')
+    res.end()
   })
 
+router.get('/choosen',async (req, res) => {
+  const { name } = req.query;
+  console.log(name);
+  const lost = await Cocktail.findOne({ title: `${name}`})
 
+  res.render('choosen', {lost})
+})
+
+router.post('/choosen', async (req, res) => {
+  const { qweryInSerch } = req.body;
+  console.log(qweryInSerch);
+  // const selectCocktail = await Cocktail.find({ title: `${qweryInSerch}` })
+  res.render('choosen', selectCocktail)
+})
+
+router.get('/:id', async (req, res, next) => {
+  const coctail = await Cocktail.findById(req.params.id)
+  console.log(coctail);
+
+  return res.render('coctail/coctail', { coctail });
+});
 
 
 
