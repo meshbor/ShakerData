5d8c169a15de220b2d37c6ac263cb3a784e89a8d
Eva <schennikova.ev@gmail.com>
2020-08-30 16:56:51 +0300

    style

--- routes/preview.js
+++ routes/preview.js
@@ -6,7 +6,7 @@ const Cocktail = require('../models/cocktail.js')
 // mongoose.connect('mongodb://localhost/cocktailBase', { useNewUrlParser: true, useUnifiedTopology: true });
 router.route('/')
   .get(async (req, res) => {
-    const allCocktall = await Cocktail.findOne({})
+    const allCocktall = await Cocktail.find({})
  
     res.render('coctail/preview', { allCocktall })
   })
