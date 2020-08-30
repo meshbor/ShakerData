2b0c2e4897f414005b0b523fb272f5b71d6bfb6b
Eva <schennikova.ev@gmail.com>
2020-08-30 16:27:38 +0300

    hi

--- seederCheerio.js
+++ seederCheerio.js
@@ -5,22 +5,25 @@ let cheerio = require('cheerio');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/GrishaProject', { useNewUrlParser: true, useUnifiedTopology: true });
 const Cocktail = require('./models/cocktail')
-
 let counter = 1;
-// async function parser(page,counter) {
   
-  let page = 20;
-while (page<50) {
-
+  let page = 100;
+while (page<600) {
 async function pars(page){
 
-  let url = `https://ru.inshaker.com/cocktails/${page}`
+let url = `https://ru.inshaker.com/cocktails/${page}`
 // парсит и сохраняет страницу
 await request(url, async (error,response,html) =>{
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
     const title = $('.common-name').text();
     const titleEn = $('.name-en').text();
+    const exist = await Cocktail.findOne({titleEng: `${titleEn}`})
+    if (exist !== null) { //  есть такой в базе, чтобы не создавать дубли
+      console.log(` такой уже есть ${exist.titleEng}`);
+    } 
+    if (exist !== null && titleEn === exist.titleEng) { console.log(` ${title} уже есть в базе `);
+      } else {
     const recipe = $('.steps').text();
     const description = $('.body').text();
     
@@ -72,12 +75,16 @@ await request(url, async (error,response,html) =>{
       description: description,
     })
     await oneCocktail.save();
-    console.log(oneCocktail);
+     await console.log(oneCocktail.title);
+  }
+}
+  else {
+  await console.log(`страница ${page} не загрузилась`);
   }
 
 });
+}
+page+=1; 
+pars(page) // чем не рекурсия
+}
 
-}
-page+=1;
-pars(page)
-}
---
+++ shop.js
@@ -0,0 +1,84 @@
+let express = require('express');
+// let app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/GrishaProject', { useNewUrlParser: true, useUnifiedTopology: true });
+const Cocktail = require('./models/cocktail')
+const Order = require('./models/order');
+
+
+let arrayofValue = [
+  ['Белый русский',10],
+  ['Бабулин сад',10],
+  ['Иностранный легион',10],
+  ['что это такое',20]
+  ['Московский мул',1],
+  ['Эль бискоти',10],
+  ['Виски сауэр',10],
+  ['Белый русский',10],
+  ['Дайкири',10],
+  ['Кровавая Мэри',10],
+  ['Иностранный легион',10],
+
+ 
+]
+
+async function fullOrder(arrayofValue) {
+  let volumeOrder = 0;
+
+for (let i = 0; i < arrayofValue.length; i++) {
+  let yourOrder = await Cocktail.findOne({ title: `${arrayofValue[i][0]}`});
+  if (yourOrder === null) { console.log(`Такого коктейля, как ${arrayofValue[i][0]} в базе нет, добавьте`);
+   } else {
+     console.log(`Коктейль ${arrayofValue[i][0]} добавлен в базу в количестве ${arrayofValue[i][1]} шт`);
+   
+  ingredientsAsk = yourOrder.ingredients; // ингредиенты коктейля в заказе 
+
+
+  for (let j = 0; j < ingredientsAsk.length; j++) {
+    let BasaIngred =  await Order.findOne({ ingredients: `${ingredientsAsk[j][0]}`})
+    // console.log(BasaIngred.volume);
+     if (BasaIngred === null) { //если такого ингредиента еще в базе нет, то создаем его
+        volumeOrder = ingredientsAsk[j][1]*arrayofValue[i][1];
+        const order = await new Order({
+        ingredients : ingredientsAsk[j][0],
+        volume: volumeOrder,
+        sizes: ingredientsAsk[j][2],
+      })
+      // console.log(order);
+      await order.save();
+     } else {
+       //иначе если такой ингредиент есть 
+      let volumer = BasaIngred.volume + (ingredientsAsk[j][1] * arrayofValue[i][1]);
+       const res = await Order.updateOne( {ingredients : `${ingredientsAsk[j][0]}` }, {$set: {volume:`${volumer}`}} ) 
+     }
+  }
+} 
+
+}
+
+}
+fullOrder(arrayofValue);
+
+
+
+// ingredients: [
+//   [ 'Выдержанный ром ', '45', 'мл' ],
+//   [ 'Дюбонне ', '15', 'мл' ],
+//   [ 'Апероль Aperol', '15', 'мл' ],
+//   [ 'Какао ликер коричневый BOLS', '5', 'мл' ],
+//   [ 'Херес манзанилья ', '15', 'мл' ],
+//   [ 'Ревеневый биттер ', '1', 'мл' ],
+//   [ 'Лимонная цедра ', '1', 'шт' ],
+//   [ 'Лед в кубиках ', '420', 'г' ]
+// ],
+// title: 'Иностранный легион',10
+// {
+//   filters: [ 'крепкие', 'кофейные', 'сливочные', 'сладкие', 'на водке' ],
+//   ingredients: [
+//     [ 'Водка Finlandia', '30', 'мл' ],
+//     [ 'Кофейный ликер BOLS', '30', 'мл' ],
+//     [ 'Нежирные сливки ', '30', 'мл' ],
+//     [ 'Лед в кубиках ', '120', 'г' ]
+//   ],
+//   title: 'Белый русский', 10
+//   titleEng: 'White Russian',
